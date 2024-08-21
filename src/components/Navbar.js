fe4c4cc1388081b441bd044b158a34ea518df467
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-21 11:19:08 +0100

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#333333', color: '#ffffff' }}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/" style={{ color: '#ffffff' }}>Online Marketplace</Link>
+                <Link className="navbar-brand" to="/" style={{ color: '#ffffff' }}>Online Marketplaceeeeeeee</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
